88d7c94e60fde43ff84a61b11d73fd4beba6a34b
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-05-24 16:56:52 -0700

    basic ui is ready

---
+++ pray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pray
+//
+//  Created by Zulwiyoza Putra on 5/24/17.
+//  Copyright © 2017 Zulwiyoza Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
